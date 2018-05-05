2bd499f4b07f456072b01f0b1abf1c93e86baac8
wuyou <444783698@qq.com>
2018-05-05 18:49:24 +0800

    基本功能

---
+++ comments/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, get_object_or_404, redirect
+
+# Create your views here.
+from blog.models import Post
+from comments.forms import CommentForm
+
+
+def post_comment(request, post_pk):
+    # 这个函数的作用是当获取的文章（Post）存在时，则获取；否则返回 404 页面给用户。
+    post = get_object_or_404(Post, pk=post_pk)
+
+    if request.method == 'POST':
+        # 用户提交的数据存在 request.POST 中，这是一个类字典对象。
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            # 将评论和被评论的文章关联起来
+            comment.post = post
+            comment.save()
+            # 重定向到 post 的详情页，实际上当 redirect 函数接收一个模型的实例时，它会调用这个模型实例的 get_absolute_url 方法，
+            # 然后重定向到 get_absolute_url 方法返回的 URL。
+            return redirect(post)
+        else:
+            # 检查到数据不合法，重新渲染详情页，并且渲染表单的错误。
+            # 因此我们传了三个模板变量给 detail.html，
+            # 一个是文章（Post），一个是评论列表，一个是表单 form
+            # 注意这里我们用到了 post.comment_set.all() 方法，
+            # 这个用法有点类似于 Post.objects.all()
+            # 其作用是获取这篇 post下的的全部评论，
+            # 因为 Post 和 Comment 是 ForeignKey 关联的，
+            # 因此使用 post.comment_set.all() 反向查询全部评论。
+            comment_list = post.comment_set.all()
+            context = {'post': post,
+                       'form': form,
+                       'comment_list': comment_list
+                        }
+            return render(request, 'blog/detail.html', context=context)
+    # 不是 post 请求，说明用户没有提交数据，重定向到文章详情页。
+    return redirect(post)
+
+
+
+
+
+
